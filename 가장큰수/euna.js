72c38648bfced4e79f66c7676570697ecc64124b
euna-dev <aheunlee0113@gmail.com>
2026-02-04 22:53:53 +0900

    fix : 괄호 추가

--- 가장큰수/euna.js
+++ 가장큰수/euna.js
@@ -1,5 +1,5 @@
 function solution(numbers) {
-  const sorted = numbers.map(String).sort((a, b) => b + a - (a + b));
+  const sorted = numbers.map(String).sort((a, b) => (b + a) - (a + b));
 
   if (sorted[0] === '0') return '0';
   return sorted.join('');
